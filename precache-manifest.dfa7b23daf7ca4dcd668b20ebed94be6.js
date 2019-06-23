d8d28f8f35180c65c21dd8ca2675a700648dc2ec
unknown <sinha.aditya244@gmail.com>
2019-06-23 11:57:22 +0530

    Updates

---
+++ precache-manifest.dfa7b23daf7ca4dcd668b20ebed94be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18be1292da2e58335cb5cda2188b4f8",
+    "url": "/react-table-data/index.html"
+  },
+  {
+    "revision": "4d655adb57b37e84089a",
+    "url": "/react-table-data/static/css/main.cb34521a.chunk.css"
+  },
+  {
+    "revision": "bc714f46bf2b415f0505",
+    "url": "/react-table-data/static/js/2.7deccb23.chunk.js"
+  },
+  {
+    "revision": "4d655adb57b37e84089a",
+    "url": "/react-table-data/static/js/main.aeedd69b.chunk.js"
+  },
+  {
+    "revision": "40d051f4a1c79e1422be",
+    "url": "/react-table-data/static/js/runtime~main.6e8218d0.js"
+  }
+]);
